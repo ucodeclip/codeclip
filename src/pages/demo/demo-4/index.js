2f3c6b48cb3e5d9ece3028e009dd91489e816189
uCodeClip <ucodeclip@gmail.com>
2020-06-20 20:32:29 +0900

    esLint 修正

--- src/pages/demo/demo-4/index.js
+++ src/pages/demo/demo-4/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Layout from "../../../components/layout/layout";
 import SEO from "../../../components/seo/seo"
 import styles from "./index.module.scss"
